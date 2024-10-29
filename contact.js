dd417d244503de712ad37c07c5e95a402c41cc8d
AbhinandanBhope <122197774+AbhinandanBhope@users.noreply.github.com>
2024-10-29 23:19:52 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,41 +1,35 @@
-const  Sequelize  = require('sequelize');
+const Sequelize = require('sequelize');
 const sequelize = require('./database');
 
-
-const customerToContact= sequelize.define('customerToContact',{
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey:true
-    },
-    name:Sequelize.STRING,
-    
-    email: {
-        type:Sequelize.STRING,
-        allowNull: false,
-        unique: true, // Make Gmail column unique
-      },
-      phoneNumber:{
-        type: Sequelize.STRING,
-  allowNull: false
-    }
-    ,
-    message:{
-        type: Sequelize.STRING,
-  allowNull: true
-    },
-    
-    active: {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false
-    },
-    
-
-    
-    
-    
+const customerToContact = sequelize.define('customerToContact', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true, // Make email column unique
+  },
+  phoneNumber: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  productHSNCode: {
+    type: Sequelize.STRING,
+    allowNull: true, // Adjust allowNull based on your requirements
+  },
+  message: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  active: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
 });
-
 
 module.exports = customerToContact;
