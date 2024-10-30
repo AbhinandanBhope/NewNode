75985a225e61bf303e9e114d2715d2609561e780
AbhinandanBhope <122197774+AbhinandanBhope@users.noreply.github.com>
2024-10-30 22:42:40 +0530

    Create customRequirement.js

---
+++ customRequirement.js
@@ -0,0 +1,43 @@
+const Sequelize = require("sequelize");
+const sequelize = require("./database");
+
+const customRequirement = sequelize.define("customRequirement", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: false,
+  },
+  phoneNumber: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  productName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  productHSNCode: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  message: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  quantity: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  active: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = customRequirement;
